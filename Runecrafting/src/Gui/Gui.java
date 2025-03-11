17868ab873c6b5600cb7194d8a9cf7a402bb019f
Terry <terrykeeton97@gmail.com>
2025-03-11 21:27:39 +0000

    Refactor Runecrafting GUI and add new altar implementations.
    Simplified and cleaned up the GUI code by removing unused components and improving structure. Added support for Earth and Fire altars with corresponding classes, enabling seamless altar selection. Updated project configuration files and removed redundant main classes from other modules.

--- Runecrafting/src/Gui/Gui.java
+++ Runecrafting/src/Gui/Gui.java
@@ -1,124 +1,43 @@
 package Gui;
 
+import RunecraftingAltar.Earth.Altar;
 import com.osmb.api.ScriptCore;
-import com.osmb.api.javafx.ItemSearchDialogue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
 public class Gui {
-    
-    private TextField itemIDTextField;
-    private TextField eatLow;
-    private TextField eatHigh;
-    private CheckBox longTapCheckbox;
-    private CheckBox foodCheckbox;
 
-    public Scene buildScene(ScriptCore core) {
-
-        VBox vBox = new VBox();
-        vBox.setSpacing(20);
+    public void buildScene(ScriptCore core) {
+        var vBox = new VBox();
+        var selectAltar = new HBox();
+        var selectAltarLabel = new Label("Select Altar");
+        var selectAltarComboBox = new ComboBox<RunecraftingAltar.Altar>();
+        
         vBox.setStyle("-fx-background-color: #636E72; -fx-padding: 10");
-        HBox selectCourseBox = new HBox();
-        selectCourseBox.setSpacing(10);
-        selectCourseBox.setAlignment(Pos.CENTER_LEFT);
+        selectAltar.setSpacing(10);
+        selectAltar.setAlignment(Pos.CENTER_LEFT);
+        selectAltarComboBox.getItems().addAll(new RunecraftingAltar.Air.Altar(), new RunecraftingAltar.Water.Altar(), new Altar(), new RunecraftingAltar.Fire.Altar());
+        selectAltarComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(RunecraftingAltar.Altar altar) {
+                return altar != null ? altar.name() : "";
+            }
 
-        Label selectCourseLabel = new Label("Select Course");
-
-        vBox.getChildren().add(selectCourseBox);
-
-
-        HBox hBox = new HBox();
-        hBox.setAlignment(Pos.CENTER_LEFT);
-        hBox.setSpacing(10);
-        Label itemIDLabel = new Label("Food item ID");
-        itemIDTextField = new TextField();
-        makeNumericOnly(itemIDTextField);
-        Button button = new Button("Find item...");
-        button.setOnAction(actionEvent -> {
-            int itemID = ItemSearchDialogue.show(core, (Stage) button.getScene().getWindow());
-            itemIDTextField.setText(String.valueOf(itemID));
-        });
-        itemIDTextField.setPrefWidth(60);
-        hBox.getChildren().addAll(itemIDLabel, itemIDTextField,button);
-
-        HBox hBox2 = new HBox();
-        hBox2.setAlignment(Pos.CENTER_LEFT);
-        hBox2.setSpacing(10);
-        Label eatBetweenLabel = new Label("Eat between");
-        eatLow = new TextField();
-        makeNumericOnly(eatLow);
-        eatLow.setText("40");
-        eatLow.setPrefWidth(60);
-        Label eatBetweenLabel2 = new Label("and");
-        eatHigh = new TextField();
-        makeNumericOnly(eatHigh);
-        eatHigh.setText("80");
-        eatHigh.setPrefWidth(60);
-        hBox2.getChildren().addAll(eatBetweenLabel, eatLow, eatBetweenLabel2, eatHigh);
-
-        VBox foodBox = new VBox(hBox, hBox2);
-        foodBox.setSpacing(10);
-        foodCheckbox = new CheckBox("Use food");
-        foodCheckbox.selectedProperty().addListener((observableValue, aBoolean, newValue) -> foodBox.setDisable(!newValue));
-        VBox foodSelectionBox = new VBox(foodCheckbox, foodBox);
-        foodSelectionBox.setSpacing(10);
-        vBox.getChildren().addAll(foodSelectionBox);
-
-        foodBox.setDisable(true);
-        longTapCheckbox = new CheckBox("Long taps");
-        longTapCheckbox.setSelected(true);
-        vBox.getChildren().add(longTapCheckbox);
-
-        Button confirmButton = new Button("Confirm");
-
-
-        vBox.getChildren().add(confirmButton);
-
-        Scene scene = new Scene(vBox);
-        scene.getStylesheets().add("style.css");
-
-        return scene;
-    }
-    
-    public int foodItemID() {
-        if (!foodCheckbox.isSelected()) {
-            return -1;
-        }
-        String itemIDString = itemIDTextField.getText();
-        if (itemIDString == null || itemIDString.isEmpty()) {
-            return -1;
-        }
-        return Integer.parseInt(itemIDString);
-    }
-
-    public int getEatLow() {
-        String eatLowString = eatLow.getText();
-        if (eatLowString == null || eatLowString.isEmpty()) {
-            return -1;
-        }
-        return Integer.parseInt(eatLowString);
-    }
-
-    public int getEatHigh() {
-        String eatHighString = eatHigh.getText();
-        if (eatHighString == null || eatHighString.isEmpty()) {
-            return -1;
-        }
-        return Integer.parseInt(eatHighString);
-    }
-
-    public boolean getLongTap() {
-        return longTapCheckbox.isSelected();
-    }
-
-    private void makeNumericOnly(TextField textField) {
-        textField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                textField.setText(newValue.replaceAll("[^\\d]", ""));
+            @Override
+            public RunecraftingAltar.Altar fromString(String string) {
+                return selectAltarComboBox.getItems().stream().filter(course -> course.name().equals(string)).findFirst().orElse(null);
             }
         });
+
+        selectAltar.getChildren().addAll(selectAltarLabel, selectAltarComboBox);
+        vBox.getChildren().add(selectAltarComboBox);
+
+        new Scene(vBox);
     }
+    
 }
